23ebcb139aaf4644cef8cbbdfb4f882ab6a2840a
Badhon <prashomanchakrabarti1@gmail.com>
2023-05-03 00:10:58 +0600

    created readma.md file and some text bollet point

---
+++ README.md
@@ -0,0 +1,25 @@
+# Assingment10 Starter Repository
+
+# This project Indian-chef-recipe
+
+A simple React app built with React & React Router Dom.
+
+# Project overview
+
+At first create vite react project then react router dom .Then created server side express js and implements server related works.api made api get methods link
+
+## css framework use to
+
+Tailwind ->[link](https://tailwindcss.com/)
+
+## Use React awesome components
+
+->[link](https://heroicons.com/)
+
+## Api :
+
+I create a slider api rendom 3 items.then install server side and api implements server side.
+
+## Live Link
+
+Hosted in firebase -> [Indian-chef-recipe]()
--- src/pages/home/Menu.jsx
+++ src/pages/home/Menu.jsx
@@ -5,46 +5,57 @@ import { Navigation, Pagination, Scrollbar, A11y } from "swiper";
 
 // Import Swiper styles
 import "swiper/css";
+import Spinner from "../spinner/Spinner";
 const Menu = () => {
   const [sliderData, setSliderData] = useState([]);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     fetch("https://indian-chef-recipe-server.vercel.app/slider")
       .then((res) => res.json())
-      .then((data) => setSliderData(data));
+      .then((data) => {
+        setSliderData(data);
+        setLoading(false);
+      });
   }, []);
   //console.log(sliderData);
   return (
     <div>
-      <Swiper
-        modules={[Navigation, Pagination, A11y]}
-        spaceBetween={20}
-        slidesPerView={1}
-        navigation
-        pagination={{ clickable: true }}
-        onSwiper={(swiper) => console.log(swiper)}
-        onSlideChange={() => console.log("slide change")}
-      >
-        {sliderData.map((slide) => (
-          <SwiperSlide key={slide.id}>
-            <div className="w-full h-auto relative">
-              <img className="w-full h-96 lg:h-screen" src={slide.img} />
-              <div className="absolute top-10 lg:top-40 lg:w-3/5 bottom-0 px-4 lg:px-32">
-                <h5 className="text-yellow-50 text-xl lg:text-4xl font-sans font-bold">
-                  {slide.title}
-                </h5>
-                <p className="text-yellow-50 sm:text-xs  mt-4">
-                  {slide.description}
-                </p>
-                <button className="me-3 px-5 py-3 bg-orange-500 rounded text-white mt-8">
-                  Read More
-                </button>
-              </div>
-            </div>
-          </SwiperSlide>
-        ))}
-        <div className="mt-5"></div>
-      </Swiper>
+      {loading ? (
+        <Spinner></Spinner>
+      ) : (
+        <>
+          <Swiper
+            modules={[Navigation, Pagination, A11y]}
+            spaceBetween={20}
+            slidesPerView={1}
+            navigation
+            pagination={{ clickable: true }}
+            onSwiper={(swiper) => console.log(swiper)}
+            onSlideChange={() => console.log("slide change")}
+          >
+            {sliderData.map((slide) => (
+              <SwiperSlide key={slide.id}>
+                <div className="w-full h-auto relative">
+                  <img className="w-full h-96 lg:h-screen" src={slide.img} />
+                  <div className="absolute top-10 lg:top-40 lg:w-3/5 bottom-0 px-4 lg:px-32">
+                    <h5 className="text-yellow-50 text-xl lg:text-4xl font-sans font-bold">
+                      {slide.title}
+                    </h5>
+                    <p className="text-yellow-50 sm:text-xs  mt-4">
+                      {slide.description}
+                    </p>
+                    <button className="me-3 px-5 py-3 bg-orange-500 rounded text-white mt-8">
+                      Read More
+                    </button>
+                  </div>
+                </div>
+              </SwiperSlide>
+            ))}
+            <div className="mt-5"></div>
+          </Swiper>
+        </>
+      )}
     </div>
   );
 };
